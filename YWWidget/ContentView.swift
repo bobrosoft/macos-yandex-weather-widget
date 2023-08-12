ecf215929ab8e32ca4aefc970e3635b1170f0a88
bobrosoft <bobrosoft@yandex.ru>
2023-08-12 18:24:52 +0200

    Initial Commit

---
+++ YWWidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YWWidget
+//
+//  Created by bobr on 12/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YWWidget/YWWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  YWWidgetApp.swift
+//  YWWidget
+//
+//  Created by bobr on 12/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct YWWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
